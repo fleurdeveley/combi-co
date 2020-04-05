cd84a54cd7921466ff90fbc4572b8c54df8470df
Fleur Develey <fleurdeveley@gmail.com>
2020-04-05 17:17:28 +0200

    fix charset option for PDO

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    $bdd = new PDO ('mysql:host=localhost;dbname=combis', 'root', '');
+    $bdd = new PDO ('mysql:host=localhost;dbname=combis', 'root', '', [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8']);
 
     $query = $bdd->prepare('
         SELECT * FROM renter
