d74f452b5832f1da03a234951937169411318c08
Fleur Develey <fleurdeveley@gmail.com>
2020-04-06 16:55:33 +0200

    add try catch

--- index.php
+++ index.php
@@ -1,27 +1,33 @@
 <?php
-    $bdd = new PDO ('mysql:host=localhost;dbname=combis', 'root', '', [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8']);
+    try{
+        $bdd = new PDO ('mysql:host=localhost;dbname=combis', 'root', '', [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8', PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    
+        $query = $bdd->prepare('
+            SELECT * FROM renter
+            JOIN address ON renter.address_id = address.id
+            WHERE renter.id = 1
+        ');
+        $query->execute();
+        $renter = $query->fetch();
+        // echo'<pre>';
+        // print_r($renter);
+        // echo'</pre>';
 
-    $query = $bdd->prepare('
-        SELECT * FROM renter
-        JOIN address ON renter.address_id = address.id
-        WHERE renter.id = 1
-    ');
-    $query->execute();
-    $renter = $query->fetch();
-    // echo'<pre>';
-    // print_r($renter);
-    // echo'</pre>';
-
-    $query = $bdd->prepare('
-        SELECT * FROM model
-        JOIN version ON model.version_id = version.id
-        LIMIT 2
-    ');
-    $query->execute();
-    $models = $query->fetchAll(PDO::FETCH_ASSOC);
-    // echo'<pre>';
-    // print_r($models);
-    // echo'</pre>';
+        $query = $bdd->prepare('
+            SELECT * FROM model
+            JOIN version ON model.version_id = version.id
+            LIMIT 2
+        ');
+        $query->execute();
+        $models = $query->fetchAll(PDO::FETCH_ASSOC);
+        // echo'<pre>';
+        // print_r($models);
+        // echo'</pre>';
+        $bdd = null;
+    } catch (PDOException $e) {
+        echo 'Erreur: '.$e->getMessage().'<br/';
+        die();
+    }
  
 ?>
 
